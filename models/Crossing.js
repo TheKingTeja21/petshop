5516996988af46bb8d1e06e59ac5046a3f00126b
TheKingTeja21 <goodboyteja07@gmail.com>
2024-03-28 23:52:49 +0530

    Crosing

--- models/Crossing.js
+++ models/Crossing.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 const Crossing=  new mongoose.Schema({
     name:{type:String, required:true},
-    nails:{type:Number, required:true},
+    Nails:{type:Number, required:true},
+    Bread:{type:String, required:true},
     Color:{type:String, required:true},
     phone:{type:Number, required:true},
     Gender:{type:String, required:true},
-    Category:{type:mongoose.Schema.Types.ObjectId, ref:"Category"},
+    isAvialble:{type:Boolean, required:true},
     imageurl:{type:String, required:true},
     Age:{type:String,required:true},
     description:{type:String, required:true},
-    Fathername:{type:String, required:true},
-    Mothername:{type:String, required:true}
+    FathertBread:{type:String, required:true},
+    MotherBrxead:{type:String, required:true}.
+    userid={type:mongoose.Schema.Types.ObjectId, ref:"User"}
 },{timestamps:true})
 
 module.exports = mongoose.model("Crossing", Crossing);
