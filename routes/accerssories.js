4c522d64d546817e37c7ab82607dc8b96d77ccf5
TheKingTeja21 <goodboyteja07@gmail.com>
2024-03-31 15:49:23 +0530

    jwt

--- routes/accerssories.js
+++ routes/accerssories.js
@@ -6,7 +6,7 @@ router.post("/create",verifyAdmin,Accessorie.createAccessories)
 router.get("/getall",Accessorie.getallAccessories)
 router.get("/accoriess",Accessorie.Accessorie)
 router.get("/search/:key",Accessorie.search)
-router.delete("/delete/:id",verifyAdminAccessorie.deleteProductById)
+router.delete("/delete/:id",verifyAdmin,Accessorie.deleteProductById)
 router.get('/availble/:id',verifyAdmin,Accessorie.productAvailable)
 
 module.exports =router
