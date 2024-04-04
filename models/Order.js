9cb720a7f11ca2803284994e4f67cc04622054db
TheKingTeja21 <goodboyteja07@gmail.com>
2024-04-04 10:50:01 +0530

    order cont change

--- models/Order.js
+++ models/Order.js
@@ -4,7 +4,7 @@ const Orderschema = new mongoose.Schema({
     sellerId:{type:String,required:true},
     imageurl:{type:String,required:true},
     productId:{type:mongoose.Schema.Types.ObjectId, ref:"Product"},
-    payment_id:{type:String,required:true},
+    payment_id:{type:String,required:true,unique:true},
     quantity:{type:Number,required:true},
     subtotal:{type:Number,required:true},
     total:{type:Number,required:true},
