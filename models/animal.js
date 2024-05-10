7ece04b8532e51d8120b4cea46427a91a0a37396
TheKingTeja21 <goodboyteja07@gmail.com>
2024-05-10 15:30:45 +0530

    about image

--- models/animal.js
+++ models/animal.js
@@ -2,6 +2,7 @@ const mongoose= require('mongoose');
 const animal= new mongoose.Schema({
     title:{type:String, required:true},
     animal:{type:String, required:true},
+    imageurl:{type:String, required:true},
 
 })
 module.exports = mongoose.model("Animal", animal);
