a2f7567caeb6de77151a380be0ef02e7f7126b60
TheKingTeja21 <goodboyteja07@gmail.com>
2024-06-04 14:44:32 +0530

    commite

--- routes/aboutepet.js
+++ routes/aboutepet.js
@@ -2,6 +2,6 @@ const router= require("express").Router()
 const {verifyAdmin} =require('../middleware/verifyToken')
 const Aboutepet = require("../controllers/aboutepet")
 
-router.post("/create",Aboutepet.createaboute)
+router.post("/create",Aboutepet.createaboute);
 
 module.exports=router
