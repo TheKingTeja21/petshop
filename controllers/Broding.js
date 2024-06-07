cb1e0c72ab517c4bb580f85d6df40ccc1bdd3a29
TheKingTeja21 <goodboyteja07@gmail.com>
2024-06-07 23:26:15 +0530

    commite

--- controllers/Broding.js
+++ controllers/Broding.js
@@ -12,6 +12,18 @@ module.exports = {
             res.status(500).json(error.message)
             
         }
+    },
+    getallbroding:async (req,res)=>{
+        try {
+            const allbroding= await Broding.find()
+            if(!allbroding){
+                return res.status(500).json("not found")
+            }
+            res.status(200).json(allbroding)
+        } catch (error) {
+            res.status(500).json(error.message)
+            
+        }
     }
 
 }
--- routes/Broding.js
+++ routes/Broding.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const broding= require("../controllers/Broding")
 
 router.post("/createbroding",broding.createbroding)
+router.get("/getallbroding",broding.getallbroding)
 
 
 module.exports =router
