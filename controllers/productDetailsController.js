aeb9ae38fafc1e885e66e06f027e93db1561c738
Ganesh Thirumalapudi <ganeshofficial1101@gmail.com>
2024-07-10 18:46:50 +0530

    changes

--- controllers/productDetailsController.js
+++ controllers/productDetailsController.js
@@ -1,5 +1,5 @@
 const Product = require('../models/Product');
-const Vaccination = require('../models/Vaccination');
+// const vaccination = require('../models/vaccinationModel');
 
 let paymentStatus = {};
 
