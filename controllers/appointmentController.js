c1fcb0b3a4ecb7700cba6406dda7bbae863c9f55
Ganesh Thirumalapudi <ganeshofficial1101@gmail.com>
2024-07-12 18:21:16 +0530

    change

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -77,7 +77,7 @@ module.exports = {
     }
   },
   completeAppointment: async (req, res) => {
-    const {appointment_id  } = req.body;
+    const {appointment_id  } = req.params;
     try {
       const appointment = await Appointment.findById(appointment_id);
       if (!appointment) {
