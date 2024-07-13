e82ee5ba9c08747abc0c15d03239ab717a6ddc39
Ganesh Thirumalapudi <ganeshofficial1101@gmail.com>
2024-07-13 12:59:06 +0530

    change

--- routes/appointment.js
+++ routes/appointment.js
@@ -3,7 +3,7 @@ const appointmentController = require('../controllers/appointmentController');
 
 router.post("/create", appointmentController.createAppointment);
 router.post("/accept", appointmentController.acceptAppointment);
-router.post("/edit", appointmentController.editAppointment);
+router.put("/edit", appointmentController.editAppointment);
 router.put("/reject", appointmentController.rejectAppointment);
 router.get("/getall", appointmentController.getAppointmentsByStatus);
 router.post("/complete", appointmentController.completeAppointment);
