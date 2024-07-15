99c91eff9f321fe5f18a5c98839e4045d988bac6
Ganesh Thirumalapudi <ganeshofficial1101@gmail.com>
2024-07-15 09:42:31 +0530

    changes

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const Userschema = new mongoose.Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    // uid: { type: String, required: false, unique: true },
+    uid: { type: String, required: false },
      password: { type: String, required: true },
       // fullName: { type: String, required: true, unique: true},
     address: {
