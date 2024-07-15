cd6fb99e701cb96553f3ff696018193ba90cf019
Ganesh Thirumalapudi <ganeshofficial1101@gmail.com>
2024-07-15 14:03:43 +0530

    changes

--- controllers/deliveryController.js
+++ controllers/deliveryController.js
@@ -1,6 +1,7 @@
 const Delivery = require("../models/delivary");
 module.exports = {
   createDelivery: async (req, res) => {
+    try{
     const {
       name,
       mobileNumber,
@@ -43,5 +44,8 @@ module.exports = {
     return res.status(200).json({
       message: "Delevery created successfully",
   })
+}catch(error) {
+  return res.status(500).json({ error: error.message });
+}
 },
 }
