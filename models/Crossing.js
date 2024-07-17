0cf09e73b3970967a2912183fb0dbcf153b3a38f
Ganesh Thirumalapudi <ganeshofficial1101@gmail.com>
2024-07-17 13:38:07 +0530

    changes

--- models/Crossing.js
+++ models/Crossing.js
@@ -1,19 +1,13 @@
-const mongoose = require('mongoose');
-const {checkVideoDuration}=require('./appointment')
-const Crossing=  new mongoose.Schema({
-    Category:{type:String,required:true},
-    Breed_name:{type:String,required:true},
-    Gender:{type:String,required:true},
-    Quality:{type:String,required:true},
-    imageurl:{type: [String],
-      validate: {
-        validator: function (v) {
-          return v.length > 0 && v.length <= 4;
-        },
-        message: props => `You can upload between 1 and 4 images, but you provided ${props.value.length}!`
-      },
-      required: true,},
-    mating_video:{ 
+const mongoose = require("mongoose");
+const { checkVideoDuration } = require("./appointment");
+const Crossing = new mongoose.Schema(
+  {
+    Category: { type: String, required: true },
+    Breed_name: { type: String, required: true },
+    Gender: { type: String, required: true },
+    Quality: { type: String, required: true },
+    imageurl: { type: String, required: true },
+    mating_video: {
       type: String,
       required: false,
       validate: {
@@ -28,26 +22,29 @@ const Crossing=  new mongoose.Schema({
           return true;
         },
         message: () => `Video duration must not be more than 10 seconds!`,
-      },},
-    Breeder_Name:{type:String,required:true},
-    aadhar_Number:{type:Number,required:true},
-    Address:{type:String,required:true},
-    status:{type:String,required:true},
-    Contact_Number: {
-        type: String,
-        validate: {
-          validator: function (v) {
-            return /^\d{10}$/.test(v);
-          },
-          message: (props) => `${props.value} is not a valid 10-digit number!`,
-        },
-        required: true,
       },
-      vaccination:{type:String,required:true},
-    location:{type:String,required:true},
-    age:{type:Number,required:true},
-    Breed_Leanage:{type:String,required:true},
-    userid:{type:mongoose.Schema.Types.ObjectId, ref:"User" }
-},{timestamps:true})
+    },
+    Breeder_Name: { type: String, required: true },
+    aadhar_Number: { type: Number, required: true },
+    Address: { type: String, required: true },
+    status: { type: String, required: true },
+    Contact_Number: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return /^\d{10}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid 10-digit number!`,
+      },
+      required: true,
+    },
+    vaccination: { type: String, required: true },
+    location: { type: String, required: true },
+    age: { type: Number, required: true },
+    Breed_Leanage: { type: String, required: true },
+    userid: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Crossing", Crossing);
